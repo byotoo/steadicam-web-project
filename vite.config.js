2bf1b20f34c8877593220d90d8b492bd6ee7893c
byotoo <brightotoo1999@gmail.com>
2026-05-06 14:28:23 +0000

    chore: stop tracking node_modules

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { resolve } from 'path'
 
 export default {
+  // Add this line! Use your actual repo name between the slashes
+  base: '/steadicam-web-project/', 
+  
   root: resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true // Ensures the old build is cleared out
   },
   server: {
     port: 8080
   },
-  // Optional: Silence Sass deprecation warnings. See note below.
   css: {
      preprocessorOptions: {
         scss: {
